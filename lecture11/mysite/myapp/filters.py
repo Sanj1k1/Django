628a83d3a2b05516f872ff5b0e792f04861dbb48
Sanj1k1 <samirgali2004@mail.ru>
2025-04-12 18:49:12 +0500

    lecture 11

---
+++ lecture11/mysite/myapp/filters.py
@@ -0,0 +1,11 @@
+# filters.py
+import django_filters
+from .models import Expense, Category
+
+class ExpenseFilter(django_filters.FilterSet):
+    date = django_filters.DateFromToRangeFilter()
+    category = django_filters.ModelChoiceFilter(queryset=Category.objects.all())
+
+    class Meta:
+        model = Expense
+        fields = ['date', 'category']
